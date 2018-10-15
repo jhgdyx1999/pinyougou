aef6e4d6ed32e32c97575e0c034e8198bb314d53
jack <1742927315@qq.com>
2018-10-15 08:21:47 +0800

    商户平台:商品新增功能的图片上传、品牌、模板等

---
+++ pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/UploadController.java
@@ -0,0 +1,36 @@
+package com.pinyougou.shop.controller;
+
+import com.pinyougou.common.util.FastDFSClient;
+import com.pinyougou.entity.Result;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author:Jack
+ * @description:
+ * @Date:created in 2018/10/14,12:33
+ */
+@RestController
+public class UploadController {
+
+    @Value("${FILE_SERVER_URL}")
+    private String fileServerUrl;
+
+    @RequestMapping("/upload")
+    public Result upload(MultipartFile file) {
+        String url;
+        try {
+            FastDFSClient fastDFSClient = new FastDFSClient("classpath:config/fdfs_client.conf");
+            String extName = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
+            String serverInnerPath = fastDFSClient.uploadFile(file.getBytes(), extName);
+            //获取文件访问url
+            url = fileServerUrl+serverInnerPath;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "文件上传失败...");
+        }
+        return new Result(true, url);
+    }
+}
--- pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -1,5 +1,5 @@
 //控制层
-app.controller('goodsController', function ($scope, $controller, goodsService) {
+app.controller('goodsController', function ($scope, $controller, goodsService,uploadService,itemCatService,typeTemplateService) {
 
     $controller('baseController', {$scope: $scope});//继承
 
@@ -31,6 +31,7 @@ app.controller('goodsController', function ($scope, $controller, goodsService) {
         );
     };
 
+    $scope.entity = {goods:{},goodsDesc:{itemImages:[],customAttributeItems:null}};
     //保存
     $scope.save = function () {
         var serviceObject;//服务层对象
@@ -77,6 +78,83 @@ app.controller('goodsController', function ($scope, $controller, goodsService) {
                 $scope.paginationConf.totalItems = response.total;//更新总记录数
             }
         );
-    }
+    };
 
-});	
+    $scope.upload_entity = {color:null,url:null};
+    //上传图片
+    $scope.upload = function () {
+        uploadService.upload().success(function (response) {
+            if (response.success) {
+                alert("图片上传成功!");
+                $scope.upload_entity.url = response.message;
+            }else{
+                alert(response.message)
+            }
+        })
+    };
+
+    //添加图片至列表
+    $scope.addToList = function () {
+        $scope.entity.goodsDesc.itemImages.push( $scope.upload_entity);
+        document.getElementById("file").value = "";
+    };
+    //从列表移除图片
+    $scope.removeFromList = function (index) {
+        $scope.entity.goodsDesc.itemImages.splice(index,1);
+    };
+
+    $scope.entity.goods.category1Id = null;
+    //加载一级分类列表
+    $scope.selectItemCatList_1 = function () {
+        itemCatService.findByParentId(0).success(function (response) {
+            $scope.itemCatList_1 = response;
+        })
+    };
+
+    //加载二级分类列表
+    $scope.$watch("entity.goods.category1Id",function (newVal, oldVal) {
+        if (newVal === null) {
+            $scope.entity.goods.category2Id = null;
+            $scope.itemCatList_2 = [];
+            $scope.selectItemCatList_1();
+            return;
+        }
+        itemCatService.findByParentId(newVal).success(function (response) {
+            $scope.itemCatList_2 = response;
+        })
+    });
+    //加载三级分类列表
+    $scope.$watch("entity.goods.category2Id",function (newVal, oldVal) {
+        if (newVal === null) {
+            $scope.entity.goods.category3Id = null;
+            $scope.itemCatList_3 = [];
+            return;
+        }
+        itemCatService.findByParentId(newVal).success(function (response) {
+            $scope.itemCatList_3 = response;
+        })
+    });
+    //查询模板ID
+    $scope.$watch("entity.goods.category3Id",function (newVal, oldVal) {
+        if (newVal === null) {
+            $scope.entity.goods.typeTemplateId = null;
+            return;
+        }
+        itemCatService.findOne(newVal).success(function (response) {
+            $scope.entity.goods.typeTemplateId = response.typeId;
+        })
+    });
+    //查询品牌列表
+    $scope.brandIds = {};
+    $scope.$watch("entity.goods.typeTemplateId",function (newVal, oldVal) {
+        if (newVal === null) {
+            $scope.brandIds = null;
+            $scope.brandIds = [];
+            return;
+        }
+        typeTemplateService.findOne(newVal).success(function (response) {
+            $scope.brandIds = JSON.parse(response.brandIds);
+            $scope.entity.goodsDesc.customAttributeItems = JSON.parse(response.customAttributeItems);
+        })
+    });
+});
